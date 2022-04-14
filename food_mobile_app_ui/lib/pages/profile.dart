1cd53b51957e788ad4d8d0f338c85dc2fbded1da
AmirBayat0 <Amirbayat.dev@gmail.com>
2022-04-14 19:20:04 +0430

    User Profile Page Completed

--- food_mobile_app_ui/lib/pages/profile.dart
+++ food_mobile_app_ui/lib/pages/profile.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:line_icons/line_icons.dart';
-//
+///
 import '../controllers/navigator_controllers.dart';
 import '../controllers/wallet_controller.dart';
 import '../utils/constanst.dart';
